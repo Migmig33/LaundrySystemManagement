86ef47c2b71b47f3770b788c2788e237a8db3b64
Migmig33 <migztan66@gmail.com>
2026-02-13 17:37:20 +0800

    created new repo, added design in frontend and static CRUD

---
+++ System/Controllers/SystemViewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace System.Controllers
+{
+    public class SystemViewController : Controller
+    {
+        // GET: SystemView
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
---
+++ System/Scripts/scripts/ControllerScript.js
@@ -0,0 +1,75 @@
+﻿app.controller("controller", function ($scope, service) {
+
+
+    $scope.customerArray = [];
+
+    $scope.addFunction = function () {
+        var customerData = {
+            "Name": $scope.Name,
+            "Username": $scope.Username,
+            "Password": $scope.Password
+        }
+        $scope.customerArray.push(customerData);
+        $scope.checkDatas();
+    }
+
+    $scope.removeFunction = function () {
+        $scope.Name = " ";
+        $scope.Username = " ";
+        $scope.Password = " ";
+
+    }
+    $scope.deleteFunction = function (index) {
+        $scope.customerArray.splice(index, 1);
+
+        $scope.checkDatas();
+    }
+    $scope.updateFunction = function (index) {
+        var newData = $scope.customerArray[index];
+        newData.Name = $scope.Name;
+        newData.Username = $scope.Username;
+        newData.Password = $scope.Password;
+       
+    }   
+
+
+    $scope.checkDatas = function () {
+        if ($scope.customerArray.length >= 1) {
+            $scope.showButton = true;
+        } else {
+            $scope.showButton = false;
+        }
+    }
+
+    $scope.directLogin = function () {
+        var storedData = JSON.stringify($scope.customerArray);
+        sessionStorage.setItem("UserArray", storedData);
+        window.location.href = "/SystemView/Login";
+    }
+    $scope.getData = function () {
+        var userDatas = sessionStorage.getItem("UserArray");
+        var storedJSONs = JSON.parse(userDatas);
+        $scope.customerArray = storedJSONs;
+
+
+
+    }
+    $scope.cancelLogin = function () {
+        $scope.loggedUsername = " ";
+        $scope.loggedPassword = " ";
+    }
+
+
+    $scope.loginFunction = function () {
+        var checkLogin = $scope.customerArray.find(
+            items => items.Username == $scope.loggedUsername && items.Password == $scope.loggedPassword);
+        if (checkLogin != undefined) {
+            alert("logged in");
+            window.location.href = "/SystemView/Home";
+        } else {
+            alert("TS NOT WORKING");
+        }
+
+    }
+
+});
